b117a5818d7d7b521d4d7db30a9b04c045146c23
Dylan Russell <dylrus2@gmail.com>
2025-09-29 20:36:54 -0400

    Move images to public folder

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="mt-5">
       <div className="d-flex justify-content-center mb-5 mt-5">
-        <img src="src\assets\images\dylan.jpg" style={{ width: "20vw", borderRadius: "50%", border: "4px solid black" }}></img>
+        <img src="/dylan.jpg" style={{ width: "20vw", borderRadius: "50%", border: "4px solid black" }}></img>
       </div>
       <div style={{marginTop: '90px'}}>
         <p className="text-center roboto-900" style={{fontSize: "32px"}}>
